7bc5573c86c785bd1d55ba36e66d68892407be45
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-06-12 21:21:28 +0500

    bug-fix

--- store/admin.py
+++ store/admin.py
@@ -57,7 +57,7 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     autocomplete_fields = ['featured_product']
-    list_display = ['title', 'products_count']
+    list_display = [ 'title', 'products_count']
     search_fields = ['title']
 
     @admin.display(ordering='products_count')
